79a62bc234cbfd5168966d696aa877187cb30052
GijsV05 <gijs.veerman@student.hu.nl>
2023-06-13 13:44:16 +0200

    horizontal positions test

--- Algorithm.py
+++ Algorithm.py
@@ -5,16 +5,32 @@ import Board
 import numpy as np
 
 guesses = Board.get_guesses()
-# Elke x als er een schip zinkt haal een schip uit de dict
+# Elke keer als er een schip zinkt haal een schip uit de dict
 ships = Board.get_ships()
 grid = Board.get_grid()
+
+def get_horizontal_positions():
+    max_length = max(ships.values())
+    pos_hor = {}
+    #kijk van waar naar waar het schip geplaatst kan worden. Pak vervolgens de meest overeenkomende.
+    for lst_number in range(len(grid)):
+        row_length = len(grid[lst_number])
+        for position in range(row_length):
+            if (position + max_length) < row_length:
+                pos_hor[position] = position + max_length
+    return pos_hor
+
+def get_vertical_positions():
+    max_length = max(ships.values())
+    pos_ver = {}
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 # Het algoritme heeft een target mode voor als er nog geen hits zijn. 
 # Kijk naar de lengte van het langste schip. Probeer dan een inschatting te maken wat de beste plek is om te gokken waar de meeste mogelijkheden zouden liggen voor een hit. 
 def target_mode(ships, guesses, grid):
-    max_length = max(ships.values())
-    print(grid.shape)
+    return get_horizontal_positions()
+
 # Eerst horizontaal daarna verticaal
 
 
-target_mode(ships, guesses, grid)
+print(target_mode(ships, guesses, grid))
