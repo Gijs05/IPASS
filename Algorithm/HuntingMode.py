a2380ac91dda9b29045e6bebdd3c72094d6b29db
GijsV05 <gijs.veerman@student.hu.nl>
2023-06-16 11:58:06 +0200

    Hoofd algoritme klaar

--- Algorithm/HuntingMode.py
+++ Algorithm/HuntingMode.py
@@ -3,35 +3,34 @@ from collections import Counter
 import Game.Board as Board
 
 def get_surrounding(coordinate, grid):
-    flat_grid = list(np.array(grid).flatten())
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-    surrounding_coordinates = []
-    surrounding_letters = [letters[let] for let in range(len(letters) - 1) if letters[let + 1] == coordinate[0] or letters[let - 1] == coordinate[0]]
-    surrounding_numbers = [numbers[num] for num in range(len(numbers) - 1) if numbers[num + 1] == coordinate[1] or numbers[num - 1] == coordinate[1]]
-    [surrounding_letters.append(int(num)) for num in surrounding_numbers]
-    for coord in surrounding_letters:
-        if isinstance(coord, str) and coord + coordinate[1] in flat_grid:
-            surrounding_coordinates.append(coord + coordinate[1])
-        if isinstance(coord, int) and coordinate[0] + str(coord) in flat_grid:
-            surrounding_coordinates.append(coordinate[0] + str(coord))
-    return surrounding_coordinates
+    surrounding_hor = [row[coord] for row in grid for coord in range(len(row) - 1) if row[coord - 1] == coordinate or row[coord + 1] == coordinate]
+    surrounding_ver = [collumn[coord] for collumn in np.array(grid).T for coord in range(len(list(collumn)) - 1) if list(collumn)[coord - 1] == coordinate or list(collumn)[coord + 1] == coordinate]
+    surrounding_coordinates = [coord for coordinates in [surrounding_hor, surrounding_ver] for coord in coordinates]
+
+    viable_coordinates = []
+    for i in surrounding_coordinates:
+        for x in grid:
+            if i in x:
+                viable_coordinates.append(i)
+    return viable_coordinates
     
-def get_possibilities(coordinate, grid, guesses):
+def get_possibilities(coordinate, grid):
     flat_grid = list(np.array(grid).flatten())
     hits = [Board.get_guesses()[coord] for coord in range(len(flat_grid)) if flat_grid[coord] == 'x']
     possible = [get_surrounding(coordinate, Board.get_grid(Board.get_guesses()))]
     for possibility in hits:
         possible.append(get_surrounding(possibility, grid))
-    
+     
     flat = []
     for row in possible:
         [flat.append(coord) for coord in row]
     return flat
 
 
-def guess(coordinate, grid, guesses):
-    possible = get_possibilities(coordinate, grid, guesses)
-    possible_array = np.array(possible)
-    most_common = Counter(list(possible_array)).most_common(1)
+def guess(possible, previous):
+    new_possible = []
+    for coord in possible:
+        if coord not in previous:
+            new_possible.append(coord)
+    most_common = Counter(list(np.array(new_possible))).most_common(1)        
     return most_common[0][0]
--- Algorithm/TargetMode.py
+++ Algorithm/TargetMode.py
@@ -8,17 +8,21 @@ def get_positions(grid, ships):
     for lst_number in range(len(grid)):
         row = grid[lst_number]
         for position in row:
-            if (row.index(position) + max_length) < len(row) and '-' not in row[row.index(position) : row.index(position) + max_length]:
+            if (row.index(position) + max_length) < len(row) and '-' not in row[row.index(position) : row.index(position) + max_length]: #or 'x' not in row[row.index(position) : row.index(position) + max_length]:
                 positions.append(row[row.index(position) : row.index(position) + max_length])
     return positions
 
-def guess(grid, ships):
+def guess(grid, ships, previous):
     transposed_grid = np.array(grid).T
     reversed_grid = [list(row) for row in transposed_grid]
     horizontal = get_positions(grid, ships)
     vertical = get_positions(reversed_grid, ships)
     [horizontal.append(pos) for pos in vertical]
 
-    flat = np.array(horizontal).flatten()
-    most_common = Counter(list(flat)).most_common(1)
+    flat = list(np.array(horizontal).flatten())
+    for coord in flat:
+        if coord in previous:
+            flat.remove(coord)
+
+    most_common = Counter(list(np.array(flat))).most_common(1)
     return most_common[0][0]
