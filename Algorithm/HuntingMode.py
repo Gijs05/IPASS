468c21a79d74e24fbc0fe34cb8f541d67d4a610f
GijsV05 <gijs.veerman@student.hu.nl>
2023-06-20 15:31:36 +0200

    aangepast zodat het op de gui toegepast kan worden

--- Algorithm/HuntingMode.py
+++ Algorithm/HuntingMode.py
@@ -10,12 +10,12 @@ def get_surrounding(coordinate, grid):
     viable_coordinates = [surround for surround in surrounding_coordinates for coord in grid if surround in coord]
     return viable_coordinates
     
-def get_possibilities(coordinate, grid, grid_colors):
+def get_possibilities(grid, grid_colors):
     hits = [coord for row in grid for coord in row if grid_colors[coord] == (225, 0, 0)]
-    possible = [get_surrounding(coordinate, grid)]
+    possible = []
     for possibility in hits:
         possible.append(get_surrounding(possibility, grid))
-
+    print(possible)
     flat = []
     for row in possible:
         [flat.append(coord) for coord in row]
@@ -25,10 +25,9 @@ def get_possibilities(coordinate, grid, grid_colors):
 def guess(previous, grid_colors, coordinate):
     coords = [coord for coord in grid_colors.keys()]
     grid = [list(coords[i:i+10]) for i in range(0, 100, 10)]
-    possible = get_possibilities(coordinate, grid, grid_colors)
-    possible.remove(previous[-1]) 
-
-    print(possible)
+    possible = get_possibilities(grid, grid_colors)
+    if previous[-1] in possible:
+        possible.remove(previous[-1]) 
     most_common = Counter(possible).most_common(1)[0][0]
     return most_common, possible
 
