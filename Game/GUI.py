d79b45fc9c34cc673b80d1f94d10ef80fae757aa
GijsV05 <gijs.veerman@student.hu.nl>
2023-06-20 19:38:03 +0200

    grid groter gemaakt

--- Game/GUI.py
+++ Game/GUI.py
@@ -7,18 +7,18 @@ def get_info():
     # Get screen info
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     screen = pygame.display.Info()
-    screen_width, screen_height = screen.current_w, screen.current_h - 70
+    screen_width, screen_height = screen.current_w - 100, screen.current_h - 205
     window = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
     pygame.display.set_caption("Battleship")
 
     resolution = (screen_width, screen_height)
-    grid_size = 10  
+    grid_size = (23, 10)  
     line_width = 1
     return window, resolution, grid_size, line_width
 
 def get_square_size(resolution, grid_size, line_width):
-    square_width = ((resolution[0] - 400) / grid_size) - line_width * ((grid_size + 1) / grid_size)
-    square_height = (resolution[1] / grid_size) - line_width * ((grid_size + 1) / grid_size)
+    square_width = ((resolution[0]) / grid_size[0]) - line_width * ((grid_size[0] + 1) / grid_size[0])
+    square_height = (resolution[1] / grid_size[1]) - line_width * ((grid_size[1] + 1) / grid_size[1])
     return (square_width, square_height)
 
 def conversion(column_row, width_height, line_width):
@@ -29,8 +29,8 @@ def draw_grid(grid_size, window, resolution, line_width, grid_color):
     square_size = get_square_size(resolution, grid_size, line_width)
     for coordinate, color in grid_color.items():
         row, column = coordinate
-        width = conversion(column, square_size[1], line_width)
-        height = conversion(row, square_size[1], line_width)
-        geometry = (width, height, square_size[1], square_size[1])
+        width = conversion(column, square_size[0], line_width)
+        height = conversion(row, square_size[0], line_width)
+        geometry = (width, height, square_size[0], square_size[0])
         pygame.draw.rect(window, color, geometry)
 
