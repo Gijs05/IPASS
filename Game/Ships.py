8b267ca361df00ea9aaf4378e9884d8783e8b1ab
GijsV05 <gijs.veerman@student.hu.nl>
2023-06-20 15:32:47 +0200

    Aangepast zodat er nog maar weinig bugs zijn

--- Game/Ships.py
+++ Game/Ships.py
@@ -3,23 +3,47 @@ import pygame
 
 
 class Ship:
-    def __init__(self, x, y, width, height, color):  
+    def __init__(self, x, y, width, height, color, name):  
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
+        self.name = name
         self.drag = False
 
     def place_ship(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
 
-    def rotate(self):
+    def rotate(self, square_size):
         self.rect.width, self.rect.height = self.rect.height, self.rect.width
-
+        max_x = 10 * square_size - self.rect.width
+        max_y = 10 * square_size - self.rect.height
+        self.rect.x = max(0, min(self.rect.x, max_x) + 10)
+        self.rect.y = max(0, min(self.rect.y, max_y) + 10)
+        
+    
+    def get_coordinates(self, square_size):
+        start_col = round(self.rect.x / square_size)
+        start_row = round(self.rect.y / square_size)
+        end_col = round((self.rect.x + self.rect.width - 1) / square_size)
+        end_row = round((self.rect.y + self.rect.height - 1) / square_size)
+        new_coords = [(row, col) for row in range(start_row, end_row) for col in range(start_col, end_col)]
+        return new_coords
+    
     def move_ship(self):
         mouse_pos = pygame.mouse.get_pos()
         self.rect.x = (mouse_pos[0]// 10) * 10
         self.rect.y = (mouse_pos[1]// 10) * 10
-                    
-    def ship_event(self, event, square_size):
+    
+    def change_color(self, new_coords, coordinates):
+        coordinates[self.name] = new_coords
+        return coordinates
+
+    def snap(self, coordinates, square_size):
+        start_col, start_row = coordinates
+        start = int(start_col), int(start_row)
+        self.rect.x = start[0] * square_size + 10
+        self.rect.y = start[1] * square_size + 10
+           
+    def ship_event(self, event, square_size, coordinates):
         if event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos):
             if event.button == 1:
                 self.drag = True
@@ -27,27 +51,20 @@ class Ship:
                 self.offset_height = event.pos[1] - self.rect.y
 
             elif event.button == 3:
-                self.rotate()
-        
-        # elif event.type == pygame.MOUSEBUTTONUP:
-        #     if event.button == 1 and self.drag == True:
-        #         self.drag = False
-        #         self.move_ship()
+                self.rotate(square_size)
+                new_coords = self.get_coordinates(square_size)
+                self.change_color(new_coords, coordinates)
           
         
-        elif event.type == pygame.MOUSEBUTTONUP:
+        elif event.type == pygame.MOUSEBUTTONUP and self.drag:
             if event.button == 1:
                 self.drag = False
-                start_col = int(self.rect.x // square_size)
-                start_row = int(self.rect.y // square_size)
-                end_col = int((self.rect.x + self.rect.width - 1) // square_size)
-                end_row = int((self.rect.y + self.rect.height - 1) // square_size)
+                new_coords = self.get_coordinates(square_size)
 
-                coordinates = []
-                for row in range(start_row, end_row + 1):
-                    for col in range(start_col, end_col + 1):
-                        coordinates.append((col, row))
-                
+                start_col = round(self.rect.x / square_size)
+                start_row = round(self.rect.y / square_size)
+                self.snap((start_col, start_row), square_size)
+                self.change_color(new_coords, coordinates)
                             
         elif event.type == pygame.MOUSEMOTION:
             if self.drag:
@@ -60,25 +77,3 @@ class Ship:
                 self.rect.x = max(0, min(self.rect.x, max_x) + 10)
                 self.rect.y = max(0, min(self.rect.y, max_y) + 10)
         
-def check_sink(coordinate, ship_locations):
-    old_ships = copy.deepcopy(ship_locations)
-
-    if check_hit(coordinate, ship_locations):
-        for ships in ship_locations:
-            if coordinate in ships:
-                ships.remove(coordinate) 
-
-    for i in range(len(old_ships)):
-        if len(ship_locations[i]) < len(old_ships[i]) and len(ship_locations[i]) == 0:
-            return True, ship_locations
-    return False, ship_locations
-    
-def remove_ship(ships, new_locations, old_locations):
-    for loc in range(len(new_locations)):
-        if len(new_locations[loc]) == 0:
-            length_ship = len(old_locations[loc])
-            for key, length in ships.items():
-                if length == length_ship:
-                    del ships[key]
-                    break
-    return ships
